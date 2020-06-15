8b680441e0978767c072242f4aca23e790d9f386
katlegoJKS <katlego.malatjie@umuzi.org>
2020-06-15 16:08:24 +0200

    updated

--- model_create.py
+++ model_create.py
@@ -1,14 +1,27 @@
 import psycopg2
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-# connection = psycopg2.connect(database='prod', user='postgres', password='pass', host='localhost')
-# cursor = connection.cursor()
+PGUSER = os.environ.get('USER')
+PASSWORD = os.environ.get('USER_PASSWORD')
+HOST = os.environ.get('HOSTNAME')
 
-# query = """INSERT INTO recruits(personal_email) VALUES ('Shaun.example.com')"""
+connection = psycopg2.connect(database="prod", user=PGUSER, password=PASSWORD, host=HOST, port=5430)
+cursor = connection.cursor()
+
+query = """CREATE TABLE Recruits(
+    id SERIAL PRIMARY KEY,
+    owner_id VARCHAR(20),
+    first_name VARCHAR(20),
+    surname VARCHAR(20),
+    rocketchat_user VARCHAR(15),
+    github_name VARCHAR(20),
+    personal_email VARCHAR(20) UNIQUE,
+    cohort VARCHAR(20));"""
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass@127.0.0.1/prod'
@@ -22,7 +35,7 @@ manager.add_command('db', MigrateCommand)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
-    recruit = db.relationship('Recruit', backref='owner', lazy='dynamic')
+    recruit = db.relationship('Recruits', backref='owner', lazy='dynamic')
 
 class Recruits(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +47,8 @@ class Recruits(db.Model):
     personal_email = db.Column(db.VARCHAR(20), unique=True)
     cohort = db.Column(db.VARCHAR(20))
 
-# cursor.execute(query)
-# connection.commit()
+cursor.execute(query)
+connection.commit()
+
 if __name__=='__main__':
     manager.run()
